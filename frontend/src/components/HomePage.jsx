4efc246a8d8dce8c67b1d2b77e8e50c4c3926b57
Shwetang Yadav <yaduvanshishwetang@gmail.com>
2025-09-01 19:48:18 +0530

    checking sidebar

--- frontend/src/components/HomePage.jsx
+++ frontend/src/components/HomePage.jsx
@@ -1,10 +1,46 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import Layout from './Layout';
+import { motion } from 'framer-motion';
+import ChatList from '../pages/chatSection/ChatList';
+import { getAllUsers } from '../services/user.service';
+import useLayoutStore from '../store/layoutStore';
 
 const HomePage = () => {
+
+  const setSelectedContact =useLayoutStore(state =>state.setSelectedContact);
+  // const location = useLocation();
+  const [allUsers,setAllUsers]=useState([]);
+  const getUser = async ()=>{
+    try {
+      const result= await getAllUsers();
+      if(result.status === 'success'){
+        setAllUsers(result.data);
+      }
+    } catch (error) {
+      console.log(error)
+      
+    }
+  }
+
+  useEffect(()=> {
+    getAllUsers();
+
+  },[]) 
+  console.log(allUsers);
+
+
   return (
-    <div>
-      HomePage
-    </div>
+    <Layout>
+      <motion.div
+      initial ={{opacity :0}}
+      animate ={{opacity:1}}
+      transition={{duration: 0.5}}
+      className ='h-full'
+    >
+      <ChatList contacts={allUsers} setSelectedContact={setSelectedContact} />
+      </motion.div>
+
+    </Layout>
   )
 }
 
--- frontend/src/components/Layout.jsx
+++ frontend/src/components/Layout.jsx
@@ -3,6 +3,8 @@ import useLayoutStore from '../store/layoutStore';
 import { useLocation } from 'react-router-dom';
 import useThemeStore from '../store/themeStore';
 import { motion,AnimatePresence } from 'framer-motion';
+import ChatWindow from '../pages/chatSection/ChatWindow';
+import Sidebar from './Sidebar';
 
 
 const Layout = ({children,isThemeDialogOpen,toggleThemeDialog,isStatusPreviewOpen,statusPreviewContent}) => {
@@ -28,22 +30,103 @@ const Layout = ({children,isThemeDialogOpen,toggleThemeDialog,isStatusPreviewOpe
       <div className={`flex-1 flex overflow-hidden ${isMobile ?"flex-col":""}`}>
 
         <AnimatePresence>
-        {(!selectedContact || !isMobile)  (
+        {(!selectedContact || !isMobile)  && (
 
-          <motion.div key="chatList"
+          <motion.div key="chatlist"
+            initial ={{x:isMobile ?"-100%":0}}
+            animate={{x:0}}
+            exit={{x:"-100%"}}
+            transition={{type:"tween"}}
+            className={`w-full md:w-2/5 h-full ${isMobile ?"pb-16":""}`}
+
           >
+            {children}
+          </motion.div>
+        )}
+
+        {(selectedContact || !isMobile)  && (
+
+          <motion.div key="chatWindow"
+            initial ={{x:isMobile ?"-100%":0}}
+            animate={{x:0}}
+            exit={{x:"-100%"}}
+            transition={{type:"tween"}}
+            className={`w-full  h-full `}
+
+          >
+            <ChatWindow
+            selectedContact={selectedContact}
+            setSelectedContact={setSelectedContact}
+            isMobile={isMobile}
+
+            />
 
           </motion.div>
         )}
+
+
+
 
         </AnimatePresence>
 
 
       </div>
+        {isMobile && <Sidebar/>}
 
+        {isThemeDialogOpen && (
+          <div className='fixed inset-0 flex items-center bg-opacity-50 z-50'> 
+          <div className ={`p-6 rounded-lg`} >
+            <h2 className='text-2xl font-semibold mb-4'>
+              Choose a theme
+            </h2>
+            <div className='space-y-4'> 
+              <label className='flex items-center space-x-3 cursor-pointer' >
+                <input type="radio"
+                  value='light'
+                  checked ={theme === 'light'}
+                  onChange={()=>setTheme("light")}
+                  className='from-radio text-blue-600 '
+                
+                />
+
+                <span>
+                  Light
+                </span>
+              </label>
+
+              <label className='flex items-center space-x-3 cursor-pointer' >
+                <input type="radio"
+                  value='light'
+                  checked ={theme === 'dark'}
+                  onChange={()=>setTheme("dark")}
+                  className='from-radio text-blue-600 '
+                
+                />
+                <span> Dark </span>
+              </label>
+            </div>
+            <button 
+             onClick={toggleThemeDialog}
+             className='mt-6-w-full bg-blue-500 text-white py-2 rounded hover:bg-blue-600 transition duration-200'
+            >
+              Close
+            </button>
+
+          </div>
+        </div>
+
+        )}
+
+        {/* status preview */}
+        {isStatusPreviewOpen && (
+          <div className='fixed inset-0 flex items-center bg-black'>
+            {statusPreviewContent}
+          </div>
+
+        )}
        
     </div>
   )
 }
 
-export default Layout
+export default Layout;
